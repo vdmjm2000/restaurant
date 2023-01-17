fec466d14fa411f0a0ee30296696d7454f81281d
vdmjm2000 <101363734+vdmjm2000@users.noreply.github.com>
2023-01-17 14:48:53 +0100

    Mise à jour

--- forms/head.php
+++ forms/head.php
@@ -44,7 +44,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <a href="../index.php" class="logo d-flex align-items-center me-auto me-lg-0">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- Uncommen the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>Chef Jean-Mi<span>.</span></h1>
       </a>
