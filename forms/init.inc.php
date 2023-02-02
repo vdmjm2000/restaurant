15f72ba9802a9ffb8504c24b7812b25d4b85dedd
vdmjm2000 <101363734+vdmjm2000@users.noreply.github.com>
2023-02-02 15:02:48 +0100

    déploiement bdd Heroku

--- forms/init.inc.php
+++ forms/init.inc.php
@@ -1,8 +1,22 @@
 <?php
+ 
+
+ if (getenv('JAWSDB_URL') !== false) {
+$dbparts = parse_url(getenv('JAWSDB_URL'));
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+ } else {
+
+
 //--------- BDD
 $mysqli = new mysqli("localhost", "root", "", "restaurant");
 if ($mysqli->connect_error) die('Un problème est survenu lors de la tentative de connexion à la BDD : ' . $mysqli->connect_error);
 // $mysqli->set_charset("utf8");
+ }
  
 //--------- SESSION
 session_start();
