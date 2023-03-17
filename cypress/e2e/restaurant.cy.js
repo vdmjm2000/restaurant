d4b7b73c0d18710b77323ac30e7df7c3ebe7f05b
vdmjm2000 <101363734+vdmjm2000@users.noreply.github.com>
2023-03-17 14:08:25 +0100

    ECF-6 <suppression tests cypress exemple>

--- cypress/e2e/restaurant.cy.js
+++ cypress/e2e/restaurant.cy.js
@@ -1,6 +1,6 @@
-describe("template spec", () => {
+describe("template spec restaurant", () => {
   it("selection authentification", () => {
-    cy.visit("https://vdmjm.com/");
+    cy.visit("http://localhost/restaurant/index.php");
     cy.get(".container > .btn-book-a-table").click();
     cy.get("#pseudo").click({ force: true });
     cy.get("#pseudo").type("pp");
@@ -10,6 +10,20 @@ describe("template spec", () => {
 
   });
 });
+
+describe("template spec profile.php", () => {
+  it("Affichage et vérification page profile ", () => {
+    cy.visit("http://localhost/restaurant/index.php");
+    cy.get(".container > .btn-book-a-table").click();
+    cy.get("#pseudo").click({ force: true });
+    cy.get("#pseudo").type("pp");
+    cy.get("#mdp").click({ force: true });
+    cy.get("#mdp").type("mm");
+    cy.get('#submit_login').click({ force: true });
+
+  });
+});
+
 
 //Ignore l'erreur par une exception
 Cypress.on("uncaught:exception", (err, runnable) => {
