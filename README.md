b620e58621b40b5a87f019ed0d0d50312c4c0167
vdmjm2000 <101363734+vdmjm2000@users.noreply.github.com>
2023-05-22 15:50:08 +0200

    mise à jour fichier sql

---
+++ README.md
@@ -0,0 +1,35 @@
+# Site en PHP restaurant "Le Quai Antique"
+
+Projet de site Web pour un restaurant en PHP avec une base de données MySQL.
+
+## Description
+
+Ce projet est un site Web développé en PHP pour un restaurant. Il permet aux utilisateurs de consulter le menu, de reserver en ligne avec ou sans compte client et de gérer les réservations. Le site utilise une base de données MySQL pour stocker les informations sur les plats et des informations sur les allergies.
+
+## Fonctionnalités
+
+- Affichage du menu avec les prix et les descriptions des plats
+- Gestion des réservations avec un formulaire de réservation en ligne
+- Création compte client afin de faciliter la saisie d'information client
+- Interface d'administration pour la gestion des plats, des commandes et des réservations
+
+## Installation
+
+1. Assurez-vous d'avoir PHP et MySQL installés sur votre système.
+2. Clonez ce dépôt dans votre répertoire de serveur web. (https://github.com/vdmjm2000/restaurant.git)
+3. Importez la base de données fournie dans le fichier `bdd.sql`.
+4. Configurez les paramètres de connexion à la base de données dans le fichier `init.inc.php`.
+5. Lancez le serveur web et accédez au site dans votre navigateur.
+
+## Utilisation
+
+- Accédez à la page d'accueil du site pour consulter les plats récemment ajoutés ainsi que les différents menus proposés
+- Utilisez le formulaire de réservation pour réserver une table (bouton "Reserver une table")
+- Connectez-vous à l'interface d'administration en utilisant les identifiants ( email ; MDP) pour gérer les plats, les menus, les horaires et les réservations.
+
+
+
+## Auteurs
+
+- Jean-Michel Van Der Meeren
+
---
+++ bdd.sql
@@ -0,0 +1,152 @@
+
+
+CREATE TABLE `booking` (
+  `id_booking` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_time` int(11) NOT NULL,
+  `date_booking` date DEFAULT NULL,
+  `nbr_people` int(11) NOT NULL,
+  `time_booking` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `categorie_recipe` (
+  `id_categorie_recipe` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `menu_name` varchar(250) NOT NULL,
+  `recipe1` varchar(100) NOT NULL,
+  `recipe2` varchar(100) NOT NULL,
+  `recipe3` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `picture` (
+  `id_picture` int(11) NOT NULL,
+  `picture` int(11) NOT NULL,
+  `description` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+CREATE TABLE `recipe` (
+  `id_recipe` int(11) NOT NULL,
+  `id_categorie_recipe` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `time_booked` (
+  `id_time` int(11) NOT NULL,
+  `time_book` time NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `date_time` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+CREATE TABLE `time_table` (
+  `id_timeTable` int(11) NOT NULL,
+  `day` varchar(20) NOT NULL,
+  `open_timelunch` time NOT NULL,
+  `close_timelunch` time NOT NULL,
+  `open_timeDinner` time NOT NULL,
+  `close_timeDinner` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `civilite` tinyint(1) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `ville` varchar(20) NOT NULL,
+  `cp` int(5) NOT NULL,
+  `adresse` varchar(500) NOT NULL,
+  `tel` text NOT NULL,
+  `commentaire` varchar(500) NOT NULL,
+  `pseudo` varchar(10) NOT NULL,
+  `mdp` varchar(20) NOT NULL,
+  `admin` int(11) NOT NULL,
+  `id_booked` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id_booking`);
+
+
+ALTER TABLE `categorie_recipe`
+  ADD PRIMARY KEY (`id_categorie_recipe`);
+
+
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`id_picture`);
+
+
+ALTER TABLE `recipe`
+  ADD PRIMARY KEY (`id_recipe`);
+
+
+ALTER TABLE `time_booked`
+  ADD PRIMARY KEY (`id_time`);
+
+
+ALTER TABLE `time_table`
+  ADD PRIMARY KEY (`id_timeTable`);
+
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`,`nom`);
+
+
+
+
+ALTER TABLE `booking`
+  MODIFY `id_booking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=428;
+
+
+ALTER TABLE `categorie_recipe`
+  MODIFY `id_categorie_recipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+
+ALTER TABLE `picture`
+  MODIFY `id_picture` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `recipe`
+  MODIFY `id_recipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+
+
+ALTER TABLE `time_booked`
+  MODIFY `id_time` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+ALTER TABLE `time_table`
+  MODIFY `id_timeTable` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+COMMIT;
